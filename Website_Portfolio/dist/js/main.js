0654ddab09c2cd254d85615c6ba7f95b7b9bbe8e
Kristina Palmquist <kristina.palmquist@gmail.com>
2024-09-13 14:26:57 +0200

    Portfolio website

--- Website_Portfolio/dist/js/main.js
+++ Website_Portfolio/dist/js/main.js
@@ -1,6 +1,3 @@
-
-
-
 // // Select DOM Items
 // const menuBtn = document.querySelector('.menu-btn');
 // const menu = document.querySelector('.menu');
@@ -35,39 +32,125 @@
 //   }
 // }
 
-function runTypingEffect() {
-    const typingElement = document.querySelector('.txt-text');
-    const text = typingElement.data-words;
-    const typingDelay = typingElement.data-wait;
-  
-    typeText(text, typingElement, typingDelay);
-  }
-  
-  function typeText(text, typingElement, delay) {
-    for (let i = 0; i < text.length; i++) {
-      setTimeout(() => {
-        typingElement.textContent += text.charAt(i);
-      }, delay * i);
+const TypeWriter = function (txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+  this.firstTime = true;
+};
+
+// Type Method
+TypeWriter.prototype.type = function () {
+  // Current index of word
+  const current = this.wordIndex % this.words.length;
+  // Get full text och current word
+  const fullTxt = this.words[current];
+  // Check if deleting
+  if (this.isDeleting) {
+    // Remove char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    if (this.firstTime === true) {
+      moveCursor();
+
+      this.firstTime = false;
     }
+  } else {
+    // Add char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
-  
-  document.addEventListener('DOMContentLoaded', runTypingEffect);
 
+  // Insert txt into Element
+  this.txtElement.innerHTML = `<span class='txt'>${this.txt}</span`;
 
-//   function runTypingEffect() {
-//     const text = 'I am Brad Traversy.';
-//     const typingElement = document.getElementById('typing-text');
-//     const typingDelay = 100;
-  
-//     typeText(text, typingElement, typingDelay);
+  // Initial Type Speed
+  let typeSpeed = 300;
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  // Check if word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    // Make paus at end
+    typeSpeed = this.wait;
+    this.txtElement.classList.remove('type-cursor');
+
+    // Set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    // Move to next word
+    this.wordIndex++;
+    // Pause before start typing
+    typeSpeed = 500;
+  }
+  if (this.wordIndex === this.words.length) {
+    this.wordIndex = 1;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
+
+// Init on DOM Load
+document.addEventListener('DOMContentLoaded', init);
+// Init App
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init Typewriter
+  new TypeWriter(txtElement, words, wait);
+}
+
+const positions = ['.fourth', '.third', '.second', '.first'];
+let posIndex = 0;
+
+// const moveCursor = () => {
+//   if (posIndex < positions.length) {
+//     const pos = document.querySelector(positions[posIndex]);
+
+//     console.log(pos);
+
+//     pos.classList.add('type-cursor');
+//     setTimeout(() => pos.classList.remove('type-cursor'), 1000);
+//     posIndex++;
+//     setTimeout(() => moveCursor, 1000);
 //   }
-  
-//   function typeText(text, typingElement, delay) {
-//     for (let i = 0; i < text.length; i++) {
-//       setTimeout(() => {
-//         typingElement.textContent += text.charAt(i);
-//       }, delay * i);
-//     }
+//   const hideEl = document.querySelector('.hide');
+//   if (hideEl.classList.contains('.hide')) {
+//     hideEl.classList.remove('hide');
 //   }
-  
-//   document.addEventListener('DOMContentLoaded', runTypingEffect);
+//   document.querySelector('.txt').classList.add('type-cursor');
+// };
+
+const moveCursor = async () => {
+  if (posIndex < positions.length) {
+    console.log(posIndex);
+    const pos = document.querySelector(positions[posIndex]);
+
+    await sleep(1000);
+
+    pos.classList.add('type-cursor');
+
+    await sleep(1000);
+    pos.classList.remove('type-cursor');
+    posIndex++;
+    setTimeout(() => moveCursor, 1000);
+  }
+
+  await sleep(1000);
+  const hideEl = document.querySelector('.hide');
+  if (hideEl.classList.contains('hide')) {
+    hideEl.classList.remove('hide');
+  }
+
+  await sleep(1000);
+  const txtEl = document.querySelector('.txt');
+  console.log(txtEl)
+  txtEl.classList.add('type-cursor');
+};
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
